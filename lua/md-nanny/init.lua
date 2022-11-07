91537a1d5929913f3c41d3dee4245f90cf4f1e68
denstiny <2254228017@qq.com>
2022-11-07 15:45:42 +0800

    feat: init

--- lua/md-nanny/init.lua
+++ lua/md-nanny/init.lua
@@ -1,2 +1,5 @@
-require("md-nanny.core.mdorg")
-require('md-nanny.core.markdown').VirMarkDown_start()
+--require("md-nanny.core.mdorg")
+--require('md-nanny.core.markdown').VirMarkDown_start()
+
+utils = require('md-nanny.treesitter_utils')
+utils.test()
---
+++ lua/md-nanny/treesitter_utils/init.lua
@@ -0,0 +1,7 @@
+local ts_utils = require("vim.treesitter")
+local M = {}
+local get_iter = function()
+
+end
+
+return M
